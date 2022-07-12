391439181c4475dfc0066c4c30f7ab4eb1f2872b
Cam Moore <cammymoore07@gmail.com>
2022-07-12 12:50:33 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+[![ci-meteor-application-template-react](https://github.com/ics-software-engineering/meteor-application-template-react/actions/workflows/ci.yml/badge.svg)](https://github.com/ics-software-engineering/meteor-application-template-react/actions/workflows/ci.yml)
+
+For details, please see http://ics-software-engineering.github.io/meteor-application-template-react/
---
+++ app/imports/ui/components/StuffItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const StuffItem = ({ stuff }) => (
+  <tr>
+    <td>{stuff.name}</td>
+    <td>{stuff.quantity}</td>
+    <td>{stuff.condition}</td>
+    <td>
+      <Link to={`/edit/${stuff._id}`}>Edit</Link>
+    </td>
+  </tr>
+);
+
+// Require a document to be passed to this component.
+StuffItem.propTypes = {
+  stuff: PropTypes.shape({
+    name: PropTypes.string,
+    quantity: PropTypes.number,
+    condition: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default StuffItem;
