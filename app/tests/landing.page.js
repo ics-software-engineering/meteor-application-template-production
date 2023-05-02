7007553d588c436eaf42f45609c85b9d4f5a1dbc
Cam Moore <cammymoore07@gmail.com>
2023-05-02 10:51:17 -1000

    Changed testcafe for Landing page.

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -11,9 +11,7 @@ class LandingPage {
   async isDisplayed() {
     // From https://testcafe.io/documentation/402803/recipes/best-practices/create-helpers
     // Note that this file imports t (the test controller) from the testcafe module. You don’t need to pass t to helper functions because TestCafe can resolve the current test context and provide the correct test controller instance.
-    const waitTime = 10;
-    console.log(`Waiting ${waitTime} seconds before running LandingPage.isDisplayed().`);
-    await t.wait(waitTime * 1000).expect(this.pageSelector.exists).ok();
+    await t.expect(this.pageSelector.exists).ok();
   }
 }
 
